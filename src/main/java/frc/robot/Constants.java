70914a74334ace5110656fd09a84682ae4161dbb
frc6045 <sabrerobotics6045@gmail.com>
2025-03-20 18:44:50 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,7 +32,7 @@ public final class Constants {
     public static final int ROLLER_MOTOR_ID = 9;
     public static final int ROLLER_MOTOR_CURRENT_LIMIT = 60;
     public static final double ROLLER_MOTOR_VOLTAGE_COMP = 10;
-    public static final double ROLLER_CORAL_OUT = -.3;
+    public static final double ROLLER_CORAL_OUT = -.2;
     public static final double ROLLER_ALGAE_IN = -0.4;
     public static final double ROLLER_ALGAE_OUT = 0.4;
     public static final double ROLLER_CORAL_STACK = -1;
